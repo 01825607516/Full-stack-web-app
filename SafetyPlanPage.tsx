77f996f7fdb8a2646dec9f317d4d000f8768e7d3
Amina <hoquemoni027@gmail.com>
2026-07-28 20:10:38 +0600

    Update SafetyPlanPage.tsx

--- SafetyPlanPage.tsx
+++ SafetyPlanPage.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { SafetyPlan } from '../types';
-import { ShieldIcon } from '../icons';
+import { SafetyPlan } from './types';
+import { ShieldIcon } from './icons';
 
 type PlanSection = keyof SafetyPlan;
 
