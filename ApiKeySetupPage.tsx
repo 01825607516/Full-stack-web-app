637426a9708ab2cf115bde0b5bf1ebb0c0a53c67
Amina <hoquemoni027@gmail.com>
2026-07-28 14:47:14 +0600

    Update ApiKeySetupPage.tsx

--- ApiKeySetupPage.tsx
+++ ApiKeySetupPage.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { ShieldIcon } from '../components/icons';
+import { ShieldIcon } from '../icons';
 
 interface ApiKeySetupPageProps {
   onApiKeySet: (key: string) => void;
